7932e46e91150bf697641333018a885008ea2747
Serah Njoroge <105037236+njoroge-s@users.noreply.github.com>
2023-03-20 16:06:48 +0300

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,12 @@
 export default function createIteratorObject(report) {
-  while (report) {
-    console.log('Placeholder');
+  const employeeList = [];
+  /* eslint-disable no-unused-vars */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employee of employees) {
+      employeeList.push(employee);
+    }
   }
+  /* eslint-enable no-unused-vars */
+
+  return employeeList;
 }
