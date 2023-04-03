a083e16d203073534d936b6b4969c7b9cd1a8900
njoroge-s <sarahnjoroge2015@gmail.com>
2023-04-03 10:57:12 +0300

    bug fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)])
-    .then((result) => {
-      const array = [];
-      result.forEach((index) => {
-        if (index.status === 'fulfilled') {
-          array.push({
-            status: index.status,
-            value: index.value,
-          });
-        } else {
-          array.push({
-            status: index.status,
-            value: `Error: ${index.reason.message}`,
-          });
-        }
-      });
-      return array;
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
