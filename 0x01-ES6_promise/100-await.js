0b9cafb2d24753a43b0632a858252522a4823d41
njoroge-s <sarahnjoroge2015@gmail.com>
2023-04-03 11:04:06 +0300

    bug fix

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,17 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
+  let res = {};
+
   try {
-    const user = await createUser();
     const photo = await uploadPhoto();
-    return {
-      photo,
-      user,
-    };
-  } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    const user = await createUser();
+    res = { photo, user };
+  } catch (err) {
+    res = { photo: null, user: null };
   }
+  return res;
 }
