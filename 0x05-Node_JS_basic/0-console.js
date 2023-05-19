d67747956285d75e75060ca4804ffa75b30e2305
njoroge-s <sarahnjoroge2015@gmail.com>
2023-05-19 14:38:51 +0300

    nodejs basic

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(str) {
- console.log(str);
-}
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Serah Njoroge <https://github.com/njoroge-s>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
   
-export default displayMessage;
+  module.exports = displayMessage;
